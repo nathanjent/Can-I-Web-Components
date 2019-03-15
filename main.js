b1e7c2dfb459ebeb0791cdb48c6bccc651ae3edd
nathanjent <nathanjent@gmail.com>
2019-03-15 17:50:30 -0400

    Add employee adding form

--- main.js
+++ main.js
@@ -5,3 +5,38 @@ import { defineSimpleComponent } from '../simplecomponent.js';
 defineSimpleComponent('person-card');
 defineSimpleComponent('confirm-deny');
 
+function addEmployeeCard() {
+    const nameField = document.querySelector('input[name="namefield"]');
+    if (!nameField || !nameField.value) return;
+    const occupationField = document.querySelector('input[name="occupationfield"]');
+    if (!occupationField || !occupationField.value) return;
+    const employeeSection = document.getElementById('employeecards');
+
+    const newCard = document.createElement('person-card');
+
+    const nameSpan = document.createElement('span');
+    nameSpan.setAttribute('slot', 'person-name');
+    nameSpan.innerHTML = nameField.value;
+    newCard.appendChild(nameSpan);
+
+    const descSpan = document.createElement('span');
+    descSpan.setAttribute('slot', 'person-desc');
+    descSpan.innerHTML = occupationField.value;
+    newCard.appendChild(descSpan);
+
+    employeeSection.appendChild(newCard);
+
+    // Clear fields on success
+    nameField.value = '';
+    occupationField.value = '';
+
+    nameField.focus();
+}
+
+(() => {
+    document.querySelector('input[name="AddEmployee"')
+        .addEventListener('click', addEmployeeCard);
+
+    // Initial focus on the name field
+    document.querySelector('input[name="namefield"]').focus();
+})();
